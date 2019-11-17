726f6da1939f4c883f3c9ca4beaf7f21ee0328ea
deoraanmol <anmol.deora@outlook.com>
2019-11-17 22:34:44 +0530

    experimented some documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {AllCommunityModules} from '@ag-grid-community/all-modules';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'learnings-for-sap';
+export class AppComponent implements OnInit {
+    @ViewChild('agGrid') agGrid: AgGridAngular;
+
+    columnDefs = [
+        {headerName: 'Make', field: 'make', sortable: true, filter: true, checkboxSelection: true},
+        {headerName: 'Model', field: 'model', sortable: true, filter: true},
+        {headerName: 'Price', field: 'price', sortable: true, filter: true}
+    ];
+
+    rowData: any;
+
+    modules = AllCommunityModules;
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    ngOnInit() {
+//         this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
+        this.rowData = this.http.get('https://api.myjson.com/bins/ly7d1');
+    }
+
+    getSelectedRows() {
+    debugger
+            const selectedNodes = this.agGrid.api.getSelectedNodes();
+            const selectedData = selectedNodes.map( node => node.data );
+            const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
+            alert(`Selected nodes: ${selectedDataStringPresentation}`);
+        }
 }
